864e961e312c32594dac91bcd7ce5c5b3055bc80
hwsmm <9151706+hwsmm@users.noreply.github.com>
2024-12-30 00:10:26 +0900

    update

--- update-deps.sh
+++ update-deps.sh
@@ -20,7 +20,7 @@ fi
 done
 
 gdxurl="https://repo1.maven.org/maven2/com/badlogicgames/gdx"
-gdxver=1.12.1
+gdxver=1.13.0
 
 down_maven()
 {
@@ -39,3 +39,6 @@ down_maven $gdxurl gdx-freetype-platform $gdxver -natives-desktop
 down_maven $gdxurl gdx $gdxver
 down_maven $gdxurl gdx-platform $gdxver -natives-desktop
 
+down_maven $gdxurl gdx-jnigen-loader 2.5.2
+down_maven "$gdxurl-controllers" gdx-controllers-core 2.2.3
+down_maven "$gdxurl-controllers" gdx-controllers-desktop 2.2.3
